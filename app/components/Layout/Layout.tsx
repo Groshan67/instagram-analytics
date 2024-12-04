b326d49f2e0d315095dcb7566eb380cb53dc5bf4
Groshan67 <ghassemroshan@gmail.com>
2024-12-04 11:06:17 +0330

    initial commit

---
+++ app/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Sidebar from "./Sidebar";
+
+const Layout = ({ }) => {
+    return (
+        <div className="flex min-h-screen">
+            {/* <Sidebar />
+            <div className="flex-1">
+                <Header />
+                <main>{children}</main>
+                <Footer />
+            </div> */}
+        </div>
+    );
+};
+
+export default Layout;
